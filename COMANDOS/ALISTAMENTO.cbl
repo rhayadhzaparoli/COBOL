b95af5ac1a7e37acf8d16840da5008106ca7c296
rhayadhzaparoli <rhayadh.zaparoli@outlook.com>
2023-07-06 11:06:53 -0300

    UPDATE ALISTAMENTO

--- COMANDOS/ALISTAMENTO.cbl
+++ COMANDOS/ALISTAMENTO.cbl
@@ -10,149 +10,153 @@
        FILE SECTION.
        WORKING-STORAGE SECTION.
        01  WS-DOB.
-        05 WS-DOB-YY                   PIC 9(004) VALUE 0.
-        05 WS-DOB-MM                   PIC 99 VALUE 0.
-        05 WS-DOB-DD                   PIC 99 VALUE 0.
+        05 WS-DOB-YY                   PIC 9(04) VALUE 0.
 
        01 WS-CURR-DATE.
-        05 WS-CURR-YY                   PIC 9(004) VALUE 0.
+        05 WS-CURR-YY                   PIC 9(04) VALUE 0.
         05 WS-CURR-MM                   PIC 99 VALUE 0.
         05 WS-CURR-DD                   PIC 99 VALUE 0.
 
        01 WS-PRS-DATA.
-         05 WS-NAME                    PIC X(050) VALUE SPACES.
-         05 WS-RG                      PIC 9(010) VALUE 0.
-         05 WS-CPF                     PIC 9(011) VALUE 0.
-         05 WS-ADRESS                  PIC X(080) VALUE SPACES.
+         05 WS-NAME                    PIC X(50) VALUE SPACES.
+         05 WS-RG                      PIC 9(10) VALUE 0.
+         05 WS-CPF                     PIC 9(11) VALUE 0.
+         05 WS-ADRESS                  PIC X(80) VALUE SPACES.
 
-       77 WS-KEY                       PIC A(001) VALUE SPACES.
-       77 WS-CALC-AGE                  PIC 99.
-       77 WS-MAT                       PIC 9(010) VALUE 0.
+       01 WS-N1                        PIC 9(02)V99 VALUE 0.
+       01 WS-N2                        PIC 9(02)V99 VALUE 0.
+       01 WS-N3                        PIC 9(02)V99 VALUE 0.
+       01 WS-N4                        PIC 9(02)V99 VALUE 0.
+       77 WS-MEDIA                     PIC 99V99.
+       77 WS-KEY                       PIC X.
+       77 WS-KEY-1                     PIC A(01).
+
+       77 WS-CALC-AGE                  PIC 99    VALUE 0.
+       77 WS-CHC                       PIC 9(02) VALUE 0.
+       77 WS-MAT                       PIC 9(05).
+       77 WS-MAT-ESP                   PIC 9(03).
+
 
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-       P100-VERIFYER.
 
-            DISPLAY ' ************************************************ '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ******* ALISTAMENTO MILITAR OBRIGATORIO ******** '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ************************************************ '
-            DISPLAY ' '
-            DISPLAY ' '
+       P100-INIT.
 
-            DISPLAY ' INSIRA A DATA DE NASCIMENTO DO CANDIDATO '
-            DISPLAY '----- O FORMATO DEVE SER (AAAAMMDD) ------'
-            ACCEPT WS-DOB
-
-            MOVE FUNCTION CURRENT-DATE TO WS-CURR-DATE
-            COMPUTE WS-CALC-AGE = WS-DOB-YY - WS-CURR-YY
-            DISPLAY ' IDADE DO CANDIDATO (A): 'WS-CALC-AGE
-
-
-            IF WS-CALC-AGE >= 18 THEN
-                 PERFORM P200-REG
-            ELSE
-                 DISPLAY " IDADE NAO PERMITIDA "
-               PERFORM P900-TERMINAL
-            END-IF.
-
-
-
-
-       P200-REG.
-
-            DISPLAY ' ************************************************ '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ********** CADASTRO DO CANDIDATO (A) *********** '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ************************************************ '
-            DISPLAY ' '
-            DISPLAY ' '
-            DISPLAY " INFORME O NOME DO CANDIDATO(A) "
-            ACCEPT WS-NAME
-            DISPLAY " INFORME O RG DO CANDIDATO(A) "
-            ACCEPT WS-RG
-            DISPLAY " INFORME O CPF DO CANDIDATO(A) "
-            ACCEPT WS-CPF
-            DISPLAY " INFORME O ENDERECO DO CANDIDATO(A) "
-            ACCEPT WS-ADRESS
-            DISPLAY " ************************************************ "
-            DISPLAY ' ************************************************ '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ********* CONFIRMA OS DADOS ABAIXO ************* '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ************************************************ '
-            DISPLAY ' '
-            DISPLAY " NOME.........: "  WS-NAME
-            DISPLAY " RG...........: "  WS-RG
-            DISPLAY " CPF..........: "  WS-CPF
-            DISPLAY " ENDERECO.....: "  WS-ADRESS
-            DISPLAY ' '
-            DISPLAY ' DESEJA ALTERAR ALGUM DADO ? '
-            ACCEPT WS-KEY
-
-            IF WS-KEY = 'S' THEN
-                DISPLAY " *****  DIGITE  ****** "
-                DISPLAY "  01 PARA ALTERAR NOME "
-                DISPLAY "   02 PARA ALTERAR RG  "
-                DISPLAY "  03 PARA ALTERAR CPF  "
-                DISPLAY " 04 PARA ALTERAR ENDERECO "
-                EVALUATE WS-PRS-DATA
-                WHEN '01'
-                DISPLAY " INFORME O NOME DO CANDIDATO(A) "
-                ACCEPT WS-NAME
-
-                WHEN '02'
-                DISPLAY " INFORME O RG DO CANDIDATO(A) "
-                ACCEPT WS-RG
-
-                WHEN '03'
-                DISPLAY " INFORME O CPF DO CANDIDATO(A) "
-                ACCEPT WS-CPF
-
-                WHEN '04'
-                DISPLAY " INFORME O ENDERECO DO CANDIDATO(A) "
-                ACCEPT WS-ADRESS
-                END-EVALUATE
-            ELSE
-                PERFORM P400-MAT
-
+           DISPLAY "CADASTRAMENTO MILITAR"
+           DISPLAY "INFORME O ANO DE NASCIMENTO"
+           ACCEPT WS-DOB-YY
+           MOVE FUNCTION CURRENT-DATE TO WS-CURR-DATE
+           COMPUTE WS-CALC-AGE = FUNCTION NUMVAL(WS-CURR-YY) -
+           FUNCTION NUMVAL(WS-DOB-YY)
+           DISPLAY "IDADE : " WS-CALC-AGE
+           IF WS-CALC-AGE >= 18 THEN
+               DISPLAY "IDADE PERMITIDA"
+               GO TO P200-VALIDATON
+           ELSE
+               DISPLAY "IDADE NAO PERMITIDA"
+               GO TO P900-END.
+       P200-VALIDATON.
+           DISPLAY "DIGITE O NOME"
+           ACCEPT WS-NAME
+           DISPLAY "DIGITE O RG"
+           ACCEPT WS-RG
+           DISPLAY "DIGITE O CPF"
+           ACCEPT WS-CPF
+           DISPLAY "DIGITE O ENDERECO"
+           ACCEPT WS-ADRESS
+           DISPLAY " "
+           GO TO P250-VERIFY.
+       P250-VERIFY.
+           DISPLAY "VERIFIQUE OS DADOS"
+           DISPLAY WS-NAME
+           DISPLAY WS-RG
+           DISPLAY WS-CPF
+           DISPLAY WS-ADRESS
+           DISPLAY "DESEJA ALTERAR DADOS ?"
+           ACCEPT WS-KEY
+           IF WS-KEY = 'S' OR WS-KEY = 's' THEN
+              GO TO P300-CHC
+           ELSE
+               GO TO P400-QI.
+       P300-CHC.
+           DISPLAY "1 PARA NOME"
+           DISPLAY "2 PARA RG"
+           DISPLAY "3 PARA CPF"
+           DISPLAY "4 PARA ENDERECO"
+           DISPLAY "0 PARA SAIR"
+           ACCEPT WS-CHC.
+               EVALUATE WS-CHC
+               WHEN 1
+               DISPLAY "DIGITE O NOME"
+               ACCEPT WS-NAME
+               GO TO P250-VERIFY
+               WHEN 2
+               DISPLAY "DIGITE O RG"
+               ACCEPT WS-RG
+               GO TO P250-VERIFY
+               WHEN 3
+               DISPLAY "DIGITE O CPF"
+               ACCEPT WS-CPF
+               GO TO P250-VERIFY
+               WHEN 4
+               DISPLAY "DIGITE O ENDERECO"
+               ACCEPT WS-ADRESS
+               GO TO P250-VERIFY
+               WHEN 0
+               GO TO P900-END
+               END-EVALUATE.
+       P400-QI.
+           DISPLAY "DIGITE A NOTA DOS 4 ULTIMOS SEMESTRES"
+           DISPLAY "NOTA 1"
+           ACCEPT WS-N1
+           DISPLAY "NOTA 2"
+           ACCEPT WS-N2
+           DISPLAY "NOTA 3"
+           ACCEPT WS-N3
+           DISPLAY "NOTA 4"
+           ACCEPT WS-N4
+           COMPUTE WS-MEDIA = (WS-N1 + WS-N2 + WS-N3 + WS-N4) / 4
+           DISPLAY WS-MEDIA
+           IF WS-MEDIA >= 9.5 THEN
+               DISPLAY "Q.I. AVANCADO"
+               GO TO P600-SESSION
+           ELSE
+               GO TO P600-SESSION
            END-IF.
 
-      *P300-VERIFY.
 
+       P500-MAT.
+            DISPLAY "GERANDO A MATRICULA....."
 
+            MOVE FUNCTION RANDOM(1) TO WS-MAT
 
-            DISPLAY " GERANDO MATRICULA ........ "
-            PERFORM P400-MAT.
+            DISPLAY WS-MAT.
+       P550-MAT-ESP.
+            DISPLAY "GERANDO A MATRICULA ESPECIAL....."
 
-       P400-MAT.
-            DISPLAY ' ************************************************ '
-            DISPLAY ' ************************************************ '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ********* MATRICULA MILITAR GERADA ************* '
-            DISPLAY ' *                                              * '
-            DISPLAY ' ************************************************ '
+            MOVE FUNCTION RANDOM(1) TO WS-MAT-ESP
 
-            DISPLAY '           'FUNCTION RANDOM(WS-MAT)
-            DISPLAY ' '
+            DISPLAY WS-MAT-ESP.
 
+       P600-SESSION.
+           IF WS-MEDIA >= 9.5  THEN
+               DISPLAY "SETOR ESPECIAL"
+               DISPLAY WS-NAME
+               DISPLAY WS-RG
+               DISPLAY WS-CPF
+               DISPLAY WS-ADRESS
+               DISPLAY WS-MAT-ESP
+               GO TO P900-END
+           ELSE
+               DISPLAY "SETOR COMUM"
+               DISPLAY WS-NAME
+               DISPLAY WS-RG
+               DISPLAY WS-CPF
+               DISPLAY WS-ADRESS
+               DISPLAY WS-MAT
+               GO TO P900-END
+           END-IF.
 
-
-
-
-            DISPLAY "DESEJA SAIR ?"
-            DISPLAY "DIGITE S OU N ?"
-            DISPLAY " "
-            ACCEPT WS-KEY
-            IF WS-KEY = "N" THEN
-               PERFORM P100-VERIFYER
-
-            END-IF.
-
-       P900-TERMINAL.
+       P900-END.
+           DISPLAY "FINALIZANDO O PROGRAMA..."
            STOP RUN.
-
-
-       END PROGRAM ALISTAMENTO-MILITAR.
+           END PROGRAM ALISTAMENTO-MILITAR.
